1e25d9ba7a9ec5d4da60e645c06ac63971282e9d
kumaran R <kumaranr21ad@psnacet.edu.in>
2024-04-09 12:25:34 +0530

    final

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 #install barcode Module
 from barcode import EAN13
-from barcode.writter import ImageWriter
+from barcode.writer import ImageWriter
 
 num_of_barcodes = int(input("how many Barcodes you need ?"))
 numbers =range(num_of_barcodes)
 for i in numbers:
     id = input("Give 12-digit numbers for your barcode id: ")
-    my_code = EAN13(id,writter=ImageWriter)
+    my_code = EAN13(id, writer=ImageWriter)
     name = input("Give name to save barcodes")
     my_code.save(name)
