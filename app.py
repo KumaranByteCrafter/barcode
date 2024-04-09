60060e5ce61a733326ecf692147d0f3f742e62e9
kumaran R <kumaranr21ad@psnacet.edu.in>
2024-04-09 12:09:01 +0530

    save the barcode image

--- app.py
+++ app.py
@@ -8,3 +8,4 @@ for i in numbers:
     id = input("Give 12-digit numbers for your barcode id: ")
     my_code = EAN13(id,writter=ImageWriter)
     name = input("Give name to save barcodes")
+    my_code.save(name)
