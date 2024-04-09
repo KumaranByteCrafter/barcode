247a2f4700d6f981e982ffbb34ff23100c49cd05
kumaran R <kumaranr21ad@psnacet.edu.in>
2024-04-09 11:47:29 +0530

    looping to ask how many barcode need

--- app.py
+++ app.py
@@ -3,3 +3,8 @@ from barcode import EAN13
 from barcode.writter import ImageWriter
 
 num_of_barcodes = int(input("how many Barcodes you need ?"))
+numbers =range(num_of_barcodes)
+for i in numbers:
+    id = input("Give 12-digit numbers for your barcode id: ")
+    my_code = EAN13(id,writter=ImageWriter)
+    name = input("Give name to save barcodes")
